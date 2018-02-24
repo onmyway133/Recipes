484dbee402514bac30c0d97bfab370fdb5b9078e
Khoa Pham <onmyway133@gmail.com>
2018-02-24 22:25:50 +0100

    Add extension on UIImage to load remote image using ImageService

---
+++ Source/Library/Extensions/UIImageView+Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  UIImageView+Extensions.swift
+//  Recipes
+//
+//  Created by Khoa Pham on 24.02.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+  /// Use for load image from an url
+  ///
+  /// - Parameter url: url for download
+  func loadImage(url: URL, placeholder: UIImage? = nil) {
+    if imageService == nil {
+      imageService = ImageService(networkService: NetworkService(), cacheService: CacheService())
+    }
+
+    self.image = placeholder
+    self.imageService?.fetch(url: url, completion: { [weak self] image in
+      self?.image = image
+    })
+  }
+
+  /// Use for store property
+  private var imageService: ImageService? {
+    get {
+      return objc_getAssociatedObject(self, &AssociateKey.imageService) as? ImageService
+    }
+    set {
+      objc_setAssociatedObject(
+        self,
+        &AssociateKey.imageService,
+        newValue,
+        objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC
+      )
+    }
+  }
+}
+
+fileprivate struct AssociateKey {
+  static var imageService = 0
+}
