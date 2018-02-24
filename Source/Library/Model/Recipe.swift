3fbb9fbf5799dc6f3eae34fab66649db23a8b717
Khoa Pham <onmyway133@gmail.com>
2018-02-24 12:39:54 +0100

    Add Recipe model

--- Source/Library/Model/Recipe.swift
+++ Source/Library/Model/Recipe.swift
@@ -2,6 +2,7 @@ import Foundation
 
 /// Represent a recipe from API response
 struct Recipe: Codable {
+  /// Name of the Publisher
   let publisher: String
 
   /// Url of the recipe on Food2Fork.com
@@ -9,6 +10,8 @@ struct Recipe: Codable {
 
   /// URL of the image
   let sourceUrl: String
+
+  /// id of recipe
   let id: String
 
   /// Title of the recipe
@@ -16,6 +19,10 @@ struct Recipe: Codable {
 
   /// URL of the image
   let imageUrl: String
+
+  /// The Social Ranking of the Recipe
   let socialRank: Double
+
+  /// Base url of the publisher
   let publisherUrl: URL
 }
