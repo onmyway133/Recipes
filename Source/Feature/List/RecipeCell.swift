5bbdf0047331ea5b75cd4194ad1cef2d3fecf862
Khoa Pham <onmyway133@gmail.com>
2018-02-24 23:55:26 +0100

    Update constraints for RecipeCell

--- Source/Feature/List/RecipeCell.swift
+++ Source/Feature/List/RecipeCell.swift
@@ -25,7 +25,40 @@ final class RecipeCell: UICollectionViewCell {
 
   private(set) lazy var label: UILabel = {
     let label = UILabel()
+    label.numberOfLines = 0
 
     return label
   }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    addSubview(containerView)
+    containerView.addSubview(imageView)
+    containerView.addSubview(label)
+
+    setupConstraints()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError()
+  }
+
+  private func setupConstraints() {
+    NSLayoutConstraint.activate([
+      containerView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
+      containerView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
+      containerView.topAnchor.constraint(equalTo: topAnchor, constant: 2),
+      containerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2),
+
+      imageView.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8),
+      imageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+      imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.9),
+      imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1),
+
+      label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+      label.leftAnchor.constraint(equalTo: imageView.rightAnchor, constant: 8),
+      label.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -8)
+    ])
+  }
 }
