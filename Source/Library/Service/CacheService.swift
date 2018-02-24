49fa87819e9245708b52169b2014e7332cb1e8f2
Khoa Pham <onmyway133@gmail.com>
2018-02-24 22:38:22 +0100

    Update CacheService to load and save data

--- Source/Library/Service/CacheService.swift
+++ Source/Library/Service/CacheService.swift
@@ -7,16 +7,55 @@
 //
 
 import Foundation
+import SwiftHash
 
 /// Save and load data to memory and disk cache
 final class CacheService {
+
+  /// For get or load data in memory
+  private let memory = NSCache<NSString, NSData>()
+
+  /// The path url that contain cached files (mp3 files and image files)
+  private let diskPath: URL
+
+  /// For checking file or directory exists in a specified path
+  private let fileManager: FileManager
+
+  init(fileManager: FileManager = FileManager.default) {
+    self.fileManager = fileManager
+    do {
+      let documentDirectory = try fileManager.url(
+        for: .documentDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: true
+      )
+      diskPath = documentDirectory.appendingPathComponent("Recipes")
+
+      if !fileManager.fileExists(atPath: diskPath.path) {
+        try fileManager.createDirectory(at: diskPath, withIntermediateDirectories: false, attributes: nil)
+      }
+    } catch {
+      fatalError()
+    }
+  }
+
   /// Save data
   ///
   /// - Parameters:
   ///   - data: The data to save
   ///   - key: Key to identify cached item
   func save(data: Data, key: String) {
+    let key = MD5(key)
 
+    DispatchQueue.global().async {
+      self.memory.setObject(data as NSData, forKey: key as NSString)
+      do {
+        try data.write(to: self.filePath(key: key))
+      } catch {
+        print(error)
+      }
+    }
   }
 
   /// Load data specified by key
@@ -24,7 +63,33 @@ final class CacheService {
   /// - Parameters:
   ///   - key: Key to identify cached item
   ///   - completion: Called when operation finishes
-  func load(key: String, completion: (Data?) -> Void) {
-    completion(nil)
+  func load(key: String, completion: @escaping (Data?) -> Void) {
+    let key = MD5(key)
+
+    DispatchQueue.global().async {
+      // If object is in memory
+      if let data = self.memory.object(forKey: key as NSString) {
+        completion(data as Data)
+        return
+      }
+
+      // If object is in disk
+      if let data = try? Data(contentsOf: self.filePath(key: key)) {
+        // Set back to memory
+        self.memory.setObject(data as NSData, forKey: key as NSString)
+        completion(data)
+        return
+      }
+
+      completion(nil)
+    }
+  }
+
+  /// Convenient method to access file named key in diskPath directory
+  ///
+  /// - Parameter key: The key, this is the file name
+  /// - Returns: The file url
+  private func filePath(key: String) -> URL {
+    return diskPath.appendingPathComponent(key)
   }
 }
