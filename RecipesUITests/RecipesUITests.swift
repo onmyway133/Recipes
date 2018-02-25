f2faf4f45f07af5e6fdf0994302f805656de718e
Khoa Pham <onmyway133@gmail.com>
2018-02-25 18:25:55 +0100

    Add some UI tests

--- RecipesUITests/RecipesUITests.swift
+++ RecipesUITests/RecipesUITests.swift
@@ -9,28 +9,49 @@
 import XCTest
 
 class RecipesUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+  var app: XCUIApplication!
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+  override func setUp() {
+    super.setUp()
+    continueAfterFailure = false
+
+    app = XCUIApplication()
+  }
+
+  func testScrolling() {
+    app.launch()
+
+    let collectionView = app.collectionViews.element(boundBy: 0)
+    collectionView.swipeUp()
+    collectionView.swipeUp()
+  }
+
+  func testGoToDetail() {
+    app.launch()
+
+    let collectionView = app.collectionViews.element(boundBy: 0)
+    let firstCell = collectionView.cells.element(boundBy: 0)
+    firstCell.tap()
+  }
+
+  func testOpenInstructionInExternalWebPage() {
+    testGoToDetail()
+
+    let button = app.buttons["View instructions"]
+    button.tap()
+  }
+
+  func testSearch() {
+    app.launch()
+
+    let searchField = app.searchFields.element(boundBy: 0)
+    searchField.tap()
+
+    searchField.typeText("banana")
+
+    let collectionView = app.collectionViews.element(boundBy: 1)
+    _ = collectionView.waitForExistence(timeout: 8)
+    let firstCell = collectionView.cells.element(boundBy: 0)
+    firstCell.tap()
+  }
 }
