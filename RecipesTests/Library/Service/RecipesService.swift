5d49ff09b3df9c17d186d80be317f5e8753a666f
Khoa Pham <onmyway133@gmail.com>
2018-02-25 18:10:02 +0100

    Add json fixtures for recipes

---
+++ RecipesTests/Library/Service/RecipesService.swift
@@ -0,0 +1,9 @@
+//
+//  RecipesService.swift
+//  RecipesTests
+//
+//  Created by Khoa Pham on 25.02.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import Foundation
---
+++ RecipesTests/Mock/MockNetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  MockNetworkService.swift
+//  RecipesTests
+//
+//  Created by Khoa Pham on 25.02.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import Foundation
